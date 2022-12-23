8427bdead2cee446a3a8b3a53b03ed1fd0a47f54
Ryan DeLong <rdelong111@gmail.com>
2022-12-23 17:10:35 -0600

    commit tree a21722787b97e9de3de8d36cc426fec993317567

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,19 @@
+class PlantsController < ApplicationController
+  def index
+    render json: Plant.all
+  end
+
+  def show
+    plant = Plant.find(params[:id])
+    if plant
+      render json: plant
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  def create
+    plant = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+    render json: plant, status: :created
+  end
+end
